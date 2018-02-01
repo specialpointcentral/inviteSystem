e25b8142a96f651f8bdab8b19870988f6dbd87e7
SPC <24204518+specialpointcentral@users.noreply.github.com>
2018-02-01 11:51:45 +0800

    fix some problems
    coding is continuing

--- manage/checkOperate.php
+++ manage/checkOperate.php
@@ -115,10 +115,10 @@ function invite($email){
     $msg="<p>尊敬的{$name}，您好</p>".
         "<p>您的申请我们已经收到并且通过了审核，欢迎您加入百川PT的大家庭！</p>".
         "<p>下面的链接是您的邀请链接，请尽快注册：</p>".
-        "<a herf=\"$url\">$url</a>".
+        "<a href=\"$url\"; target=\"_blank\">$url</a>".
         "<p style=\"font-weight:blod\">下面的内容请您认真阅读：</p>".
-        "<blockquote style=\"border: 1px solid #999;padding: 1.25rem;border-left-width: .25rem;\">
-            百川PT是一个拥有丰富资源的非开放社区,烦请在加入后仔细阅读规则并确认邀请。最后,确保维持一个良好的分享率，并且分享允许的资源。
+        "<blockquote style=\"border: 1px solid #999;padding: 1.25rem;border-left-width: .25rem; margin-left: 0px\">
+            百川PT是一个拥有丰富资源的非开放社区,烦请在加入后确认邀请并<span style=\"color: red; font-weight: bold\">仔细阅读规则<span>。最后,确保维持一个良好的分享率，并且分享允许的资源。
         </blockquote>".
         "<p style=\"float:right\">百川PT管理组</p>";
     $post=array(
@@ -127,7 +127,6 @@ function invite($email){
         'title'=>base64_encode('百川PT-申请通过及确认函'),
         'msg'=>base64_encode($msg),
     );
-    request_post($sendURL,$post);
     $postTime=date("Y-m-d H:i:s",time());
     $sql="UPDATE tbl_apply SET status = '1' , checkTime = '".$postTime."' WHERE email = '".base64_encode($email)."'";
     $db->query($sql);
@@ -138,6 +137,7 @@ function invite($email){
         'msg' => '成功',
     );
     echo json_encode($echo);
+    request_post($sendURL,$post);//请求发送邮件
     exit();
 }
 
